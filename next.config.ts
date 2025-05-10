f82ebc1977467cfc2282f18d2a7f9ad68baec6ef
cybersquad123 <Aimlresources1@gmail.com>
2025-05-10 18:21:45 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,23 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**', // Allows /width/height and /id/... and /seed/... etc.
+      },
+    ],
+  },
+};
+
+export default nextConfig;
